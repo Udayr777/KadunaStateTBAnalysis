805a4f684614681a4862ba1f9bd77873ca3e8119
udayr777 <udayr.2012@gmail.com>
2024-01-17 19:28:18 -0800

    Changes has been updated
    updating the respecting files

--- block1.py
+++ block1.py
@@ -1,29 +1,32 @@
 import matplotlib.pyplot as plt
 import streamlit as st
-def vis1A(combined_df, numYears, numQuarters):
+import geopandas as gpd
+import pandas as pd
+import plotly.express as px
+def vis1A(blockCombined, numYears, numQuarters):
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(14, 10))
     
 
     # Total number of presumptives
-    axes[0, 0].hist(combined_df['Total number of presumptives'], bins=20, color='skyblue', edgecolor='black')
+    axes[0, 0].hist(blockCombined['Total number of presumptives'], bins=20, color='skyblue', edgecolor='black')
     axes[0, 0].set_title('Total number of presumptives')
     axes[0, 0].set_xlabel('Number of presumptives')
     axes[0, 0].set_ylabel('Frequency')
 
     # Total examined with Xpert
-    axes[0, 1].hist(combined_df['Total examined with Xpert'], bins=20, color='lightgreen', edgecolor='black')
-    axes[0, 1].set_title('Total examined with Xpert')
-    axes[0, 1].set_xlabel('Number examined with Xpert')
+    axes[0, 1].hist(blockCombined['Presumptive DS-TB'], bins=20, color='lightgreen', edgecolor='black')
+    axes[0, 1].set_title('Presumptive DS-TB')
+    axes[0, 1].set_xlabel('Number examined with Presumptive DS-TB')
     axes[0, 1].set_ylabel('Frequency')
 
     # MTB detected
-    axes[1, 0].hist(combined_df['MTB detected'], bins=20, color='lightcoral', edgecolor='black')
-    axes[1, 0].set_title('MTB detected')
-    axes[1, 0].set_xlabel('Number of MTB detected cases')
+    axes[1, 0].hist(blockCombined['Presumptive DR-TB'], bins=20, color='lightcoral', edgecolor='black')
+    axes[1, 0].set_title('Presumptive DR-TB')
+    axes[1, 0].set_xlabel('Number of Presumptive DR-TB detected cases')
     axes[1, 0].set_ylabel('Frequency')
 
     # Total diagnosed
-    axes[1, 1].hist(combined_df['Total diagnosed'], bins=20, color='gold', edgecolor='black')
+    axes[1, 1].hist(blockCombined['Total diagnosed'], bins=20, color='gold', edgecolor='black')
     axes[1, 1].set_title('Total diagnosed')
     axes[1, 1].set_xlabel('Number of diagnosed cases')
     axes[1, 1].set_ylabel('Frequency')
@@ -31,6 +34,3 @@ def vis1A(combined_df, numYears, numQuarters):
 
     st.pyplot(fig)
 
-    if(numYears + numQuarters > 2):
-        st.write("Graph time")
-   
